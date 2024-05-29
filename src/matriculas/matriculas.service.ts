cc7712256d9a3610fc5dfbfad867c955e05bab53
arielGs_Dev <arielogavilanes1534@gmail.com>
2024-05-28 23:54:52 -0500

    redoing

--- src/matriculas/matriculas.service.ts
+++ src/matriculas/matriculas.service.ts
@@ -1,14 +1,4 @@
-import { Injectable} from '@nestjs/common'; 
-import { Curso } from 'src/cursos/interface/cursos.interface'; 
-import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class MatriculasService {
-  constructor(private http: HttpService) {}
-
-  async getCursosPorEstudiante(idEstudiante: number): Promise<Curso[]> {
-    const response = await this.http.get(`https://blog.hubspot.com/website/api-endpoint/matriculas/courses/${idEstudiante}`).toPromise();
-    return response.data as Curso[]; 
-  }
-}
-
+export class MatriculasService {}
