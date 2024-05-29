bfdcf8c0e11f9a33088b0a6d9fc864ebd428faba
DilanJAG <dilanarocajoe@gmail.com>
2024-05-28 21:32:06 -0500

    creación de matriculas

--- src/matriculas/matriculas.controller.ts
+++ src/matriculas/matriculas.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { MatriculasService } from './matriculas.service';
+import { Matriculas } from './entity/matriculas.entity';
 
 @Controller('matriculas')
-export class MatriculasController {}
+export class MatriculasController {
+  constructor(private matriculasService: MatriculasService) {}
+
+  @Post()
+  async crearMatricula(@Body() matricula: Matriculas): Promise<any> {
+    return this.matriculasService.crearMatricula(matricula);
+  }
+}
